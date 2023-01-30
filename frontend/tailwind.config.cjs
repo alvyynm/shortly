292e236a65b3118bb97639d0045125c6f3be3119
alvyynm <alvyynm@gmail.com>
2023-01-30 21:40:41 +0300

    style: add custom colors

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: 'hsl(180, 66%, 49%)',
+        'dark-violet': 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        gray: 'hsl(0, 0%, 75%)',
+        'grayish-violet': 'hsl(257, 7%, 63%)',
+        'dark-blue': 'hsl(255, 11%, 22%)',
+        'dark-violet': 'hsl(260, 8%, 14%)',
+      },
+    },
   },
   plugins: [],
 };
