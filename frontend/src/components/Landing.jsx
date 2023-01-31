21ea553102c7309c592c7971c9cb733cbe0e0a61
alvyynm <alvyynm@gmail.com>
2023-01-31 16:18:15 +0300

    chore: add JSX content for hero section

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import heroimg from '../assets/illustration-working.svg';
+
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <section>
+      {/* // HERO SECTION */}
+      <div>
+        <div>
+          <h1>
+            More than just <span>shorter links</span>{' '}
+          </h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <div>
+          <img
+            src={heroimg}
+            alt="illustration of person working on a computer"
+          />
+        </div>
+      </div>
+
+      {/* INPUT SECTION */}
+      {/* STATS SECTION */}
+      {/* CALLOUT SECTION */}
+    </section>
+  );
 }
 
 export default Landing;
