45cfcb34b40db80b96d69bbb54f3169ca234b772
alvyynm <alvyynm@gmail.com>
2023-02-01 22:12:06 +0300

    chore(content): add JSX content for Stats section

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import heroimg from '../assets/illustration-working.svg';
+import brandrecog from '../assets/icon-brand-recognition.svg';
+import detailrec from '../assets/icon-detailed-records.svg';
+import fullycus from '../assets/icon-fully-customizable.svg';
 
 function Landing() {
   return (
@@ -53,8 +56,59 @@ function Landing() {
           </button>
         </form>
       </div>
+
       {/* STATS SECTION */}
+
+      <div>
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+        <div>
+          <div>
+            <div>
+              <img src={brandrecog} alt="" />
+            </div>
+            <h3>Brand Recognition</h3>
+            <p>
+              Boost your brand recognition with each click.Generic links don’t
+              mean a thing. Branded links help instil confidence in your
+              content.
+            </p>
+          </div>
+          <div>
+            <img src={detailrec} alt="" />
+            <div>
+              <h3>Detailed Records</h3>
+            </div>
+            <p>
+              Gain insights into who is clicking your links. Knowing when and
+              where people engage with your content helps inform better
+              decisions.
+            </p>
+          </div>
+          <div>
+            <div>
+              <img src={fullycus} alt="" />
+            </div>
+            <h3>Fully Customizable</h3>
+            <p>
+              Improve brand awareness and content discoverability through
+              customizable links, supercharging audience engagement.
+            </p>
+          </div>
+        </div>
+      </div>
+
       {/* CALLOUT SECTION */}
+
+      <div>
+        <div>
+          <h2>Boost your links today</h2>
+          <button>Get Started</button>
+        </div>
+      </div>
     </section>
   );
 }
