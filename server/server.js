2577c03a288f9b35747a5a6d3d13f9c021f60485
alvyynm <alvyynm@gmail.com>
2023-02-04 12:39:59 +0300

    chore(setup): setup backend server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-/// Nothing
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+
+const shortcodeRoutes = require("./routes/shortcode");
+
+const app = express();
+
+app.use("/api.shortly/v1/", shortcodeRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
