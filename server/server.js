934b02901d4ce7294fda7dc5b3eeca6c60f0a91a
alvyynm <alvyynm@gmail.com>
2023-02-06 23:29:09 +0300

    feat(backend): prevent CORS errors

--- server/server.js
+++ server/server.js
@@ -9,6 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Prevent cors errors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api.shortly/v1/", shortcodeRoutes);
 
 app.listen(port, () => {
