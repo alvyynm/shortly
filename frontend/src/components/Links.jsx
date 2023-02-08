b7fa8d53c8b97b3ee56da69bdba388792b0ac0b3
alvyynm <alvyynm@gmail.com>
2023-02-08 22:45:48 +0300

    chore: make values displayed in Links dynamic

--- frontend/src/components/Links.jsx
+++ frontend/src/components/Links.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 
-function Links() {
+function Links({ originalUrl, shortUrl }) {
   return (
     <div className="py-4">
       <div className="rounded-xl flex flex-col items-center lg:justify-between lg:flex-row py-3 px-3 bg-white">
         <div>
-          <p className="text-dark-violet-deep">frontendmentor.io</p>
+          <p className="text-dark-violet-deep">
+            {originalUrl || 'frontendmentor.io'}
+          </p>
         </div>
         <div>
           <p className="flex flex-row items-center gap-4">
             <a
               className="text-cyan hover:text-[#9ee0e1]"
-              href="https://shiny.link/XhzYaX"
+              href={shortUrl || `https://shiny.link/XhzYaX`}
               target="_blank"
             >
-              https://shiny.link/XhzYaX
+              {shortUrl || 'https://shiny.link/XhzYaX'}
             </a>
             <button className="text-white bg-cyan hover:bg-[#9ee0e1] px-5 py-1 rounded-md">
               Copy
